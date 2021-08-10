835641a6f9ad21d152bbf4ea8f8e5d7036163140
mritd <mritd@linux.com>
2021-08-10 16:52:52 +0800

    fix(alpine): add quem
    add quem
    
    Signed-off-by: mritd <mritd@linux.com>

--- earthfiles/alpine/build.sh
+++ earthfiles/alpine/build.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+function install_pkg(){
+    sudo apt install jq qemu binfmt-support qemu-user-static -y
+}
+
 function init_earthly(){
      sudo wget -q -O /usr/local/bin/earthly $(curl -sL https://api.github.com/repos/earthly/earthly/releases/latest \
          | jq -r '.assets[].browser_download_url' | grep earthly-linux-amd64)
@@ -12,5 +16,6 @@ function build(){
     earthly -P --push +all
 }
 
+install_pkg
 init_earthly
 build
