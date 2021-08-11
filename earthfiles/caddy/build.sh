3828c10c7518dbbc61a3ebf9ce2c72dc02b8854e
mritd <mritd@linux.com>
2021-08-11 11:21:48 +0800

    feat(caddy): update build
    update build
    
    Signed-off-by: mritd <mritd@linux.com>

--- earthfiles/caddy/build.sh
+++ earthfiles/caddy/build.sh
@@ -17,6 +17,7 @@ function build(){
     earthly -P --build-arg VERSION=$(cat version) --push +all
     git config --global user.name mritd
     git config --global user.email mritd@linux.com
+    git pull https://mritd:${COMMIT_TOKEN}@github.com/mritd/autobuild.git
     git add version
     git commit -m "$(cat version): GitHub Action Auto Update(`date +'%Y-%m-%d %H:%M:%S'`)"
     git push https://mritd:${COMMIT_TOKEN}@github.com/mritd/autobuild.git
