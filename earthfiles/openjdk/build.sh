c2fa5b3a2933a792df0a8599ce21b96cb5554d90
mritd <mritd@linux.com>
2021-08-12 16:40:23 +0800

    feat(openjdk): add openjdk
    add openjdk
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ earthfiles/openjdk/build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -e
+
+function install_pkg(){
+    sudo apt install jq qemu binfmt-support qemu-user-static -y
+}
+
+function init_earthly(){
+     sudo wget -q -O /usr/local/bin/earthly $(curl -sL https://api.github.com/repos/earthly/earthly/releases/latest \
+         | jq -r '.assets[].browser_download_url' | grep earthly-linux-amd64)
+     sudo chmod +x /usr/local/bin/earthly
+}
+
+function build(){
+    earthly -P --push +all
+}
+
+install_pkg
+init_earthly
+build
