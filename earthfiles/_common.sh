6121397b35b227b947dc4f5eec44b4628944328c
mritd <mritd@linux.com>
2021-09-19 20:51:38 +0800

    feat(script): add common.sh
    add common.sh
    
    Signed-off-by: mritd <mritd@linux.com>

---
+++ earthfiles/_common.sh
@@ -0,0 +1,11 @@
+function install_pkg(){
+    sudo apt install jq qemu binfmt-support qemu-user-static -y
+}
+
+function init_earthly(){
+    if ! command -v earthly >/dev/null 2>&1; then
+        sudo wget -q -O /usr/local/bin/earthly $(curl -sL https://api.github.com/repos/earthly/earthly/releases/latest \
+            | jq -r '.assets[].browser_download_url' | grep earthly-linux-amd64)
+        sudo chmod +x /usr/local/bin/earthly
+    fi
+}
--- earthfiles/certmonitor/build.sh
+++ earthfiles/certmonitor/build.sh
@@ -2,16 +2,7 @@
 
 set -e
 
-function install_pkg(){
-    sudo apt install jq qemu binfmt-support qemu-user-static -y
-}
-
-
-function init_earthly(){
-     sudo wget -q -O /usr/local/bin/earthly $(curl -sL https://api.github.com/repos/earthly/earthly/releases/latest \
-         | jq -r '.assets[].browser_download_url' | grep earthly-linux-amd64)
-     sudo chmod +x /usr/local/bin/earthly
-}
+source ../_common.sh
 
 function build(){
     earthly --push +all
