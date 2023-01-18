07214696acd20a52b4b7bc3cc7892c330821984f
kovacs <mritd@linux.com>
2023-01-19 00:22:38 +0800

    fix(dnsacme): fix unraid
    fix unraid
    
    Signed-off-by: kovacs <mritd@linux.com>

--- dnsacme/unraid.sh
+++ dnsacme/unraid.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-SSH_COMD="ssh -i /data/ssh_dnsacme -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR root@127.0.0.1"
+SSH_CMD="ssh -i /data/ssh_dnsacme -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR root@127.0.0.1"
 CERT_PATH="/host_certs/$(${SSH_CMD} uname -n)_unraid_bundle.pem"
 
 cat ${ACME_CERT_PATH} > ${CERT_PATH}
