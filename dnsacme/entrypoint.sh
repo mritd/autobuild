454cae4ba48139e35df0c7f2af6ebd58c696fa0f
kovacs <mritd@linux.com>
2023-01-18 22:01:49 +0800

    feat(dnsacme): fix unraid script
    fix unraid script
    
    Signed-off-by: kovacs <mritd@linux.com>

--- dnsacme/entrypoint.sh
+++ dnsacme/entrypoint.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-if [ ! -f /data/dnsacme ] || [ ! -f /data/dnsacme.pub ]; then
-    ssh-keygen -t ed25519 -f /data/dnsacme -q -C dnsacme -N "" <<<y >/dev/null 2>&1
+if [ ! -f /data/ssh_dnsacme ] || [ ! -f /data/ssh_dnsacme.pub ]; then
+    ssh-keygen -t ed25519 -f /data/ssh_dnsacme -q -C dnsacme -N "" <<<y >/dev/null 2>&1
     echo >> /host_ssh/authorized_keys
-    cat /data/dnsacme.pub >> /host_ssh/authorized_keys
+    cat /data/ssh_dnsacme.pub >> /host_ssh/authorized_keys
 fi
 
 exec dnsacme
