f4e97e5532d18229944a7903f91dcd6c37773d74
kovacs <mritd@linux.com>
2023-01-19 00:43:23 +0800

    feat(dnsacme): update unraid
    update unraid
    
    Signed-off-by: kovacs <mritd@linux.com>

--- dnsacme/unraid.sh
+++ dnsacme/unraid.sh
@@ -5,7 +5,6 @@ set -e
 SSH_CMD="ssh -i /data/ssh_dnsacme -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -o LogLevel=ERROR root@127.0.0.1"
 CERT_PATH="/host_certs/$(${SSH_CMD} uname -n)_unraid_bundle.pem"
 
-cat ${ACME_CERT_PATH} > ${CERT_PATH}
-cat ${ACME_KEY_PATH} >> ${CERT_PATH}
+cat ${ACME_CERT_PATH} ${ACME_KEY_PATH} > ${CERT_PATH}
 
 ${SSH_CMD} /etc/rc.d/rc.nginx reload
