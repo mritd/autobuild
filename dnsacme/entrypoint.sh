dcf1a023d4b0a9788df2b95406dacf4bfafbadbb
kovacs <mritd@linux.com>
2023-01-18 17:53:15 +0800

    feat(dnsacme): update unraid entrypoint
    update unraid entrypoint
    
    Signed-off-by: kovacs <mritd@linux.com>

--- dnsacme/entrypoint.sh
+++ dnsacme/entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -q -C dnsacme -N ""
+ssh-keygen -t ed25519 -f /root/.ssh/id_ed25519 -q -C dnsacme -N "" <<<y >/dev/null 2>&1
 cat /root/.ssh/id_ed25519.pub >> /host_ssh/authorized_keys
 
 exec dnsacme
