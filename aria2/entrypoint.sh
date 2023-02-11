663b782d6b70f416da106a7e3cefed3ac2aa1762
kovacs <mritd@linux.com>
2023-02-11 18:52:12 +0800

    feat(aria2): update config
    update config
    
    Signed-off-by: kovacs <mritd@linux.com>

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 TRACKER_URL="https://cdn.staticaly.com/gh/XIU2/TrackersListCollection/master/best_aria2.txt"
 
 if [ ! -f /data/conf/aria2.conf ]; then
-    mkdir -p /data/conf
+    mkdir -p /data/{conf,downloads}
     touch /data/conf/aria2.session
     cp /etc/aria2.conf /data/conf/aria2.conf
     sed -i "s@bt-tracker=@bt-tracker=$(curl -sSL ${TRACKER_URL})@g" /data/conf/aria2.conf
