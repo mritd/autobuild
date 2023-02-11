ff8c644205455079e3560c8a70785cf599305809
kovacs <mritd@linux.com>
2023-02-11 19:11:02 +0800

    fix(aria2): fix config
    fix config
    
    Signed-off-by: kovacs <mritd@linux.com>

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -8,6 +8,7 @@ if [ ! -f /data/conf/aria2.conf ]; then
     mkdir -p ${XDG_DATA_HOME}/{conf,downloads}
     touch ${XDG_DATA_HOME}/conf/aria2.session
     cp /etc/aria2.conf ${XDG_DATA_HOME}/conf/aria2.conf
+    sed -i "s@XDG_DATA_HOME@${XDG_DATA_HOME}@g" ${XDG_DATA_HOME}/conf/aria2.conf
     sed -i "s@bt-tracker=@bt-tracker=$(curl -sSL ${TRACKER_URL})@g" ${XDG_DATA_HOME}/conf/aria2.conf
 fi
 
