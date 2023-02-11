e9967add6d1e31b876055acbc92498e6208a92b1
kovacs <mritd@linux.com>
2023-02-11 19:06:31 +0800

    feat(aria2): add XDG_DATA_HOME
    add XDG_DATA_HOME
    
    Signed-off-by: kovacs <mritd@linux.com>

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -5,10 +5,10 @@ set -e
 TRACKER_URL="https://cdn.staticaly.com/gh/XIU2/TrackersListCollection/master/best_aria2.txt"
 
 if [ ! -f /data/conf/aria2.conf ]; then
-    mkdir -p /data/{conf,downloads}
-    touch /data/conf/aria2.session
-    cp /etc/aria2.conf /data/conf/aria2.conf
-    sed -i "s@bt-tracker=@bt-tracker=$(curl -sSL ${TRACKER_URL})@g" /data/conf/aria2.conf
+    mkdir -p ${XDG_DATA_HOME}/{conf,downloads}
+    touch ${XDG_DATA_HOME}/conf/aria2.session
+    cp /etc/aria2.conf ${XDG_DATA_HOME}/conf/aria2.conf
+    sed -i "s@bt-tracker=@bt-tracker=$(curl -sSL ${TRACKER_URL})@g" ${XDG_DATA_HOME}/conf/aria2.conf
 fi
 
-exec aria2c --conf-path=/data/conf/aria2.conf
+exec aria2c --conf-path=${XDG_DATA_HOME}/conf/aria2.conf
