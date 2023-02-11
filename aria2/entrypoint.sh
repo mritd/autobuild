aa45313d3ffb9cef881f28cd4e339434bfd97e12
kovacs <mritd@linux.com>
2023-02-11 18:43:41 +0800

    feat(aria2): add tracker update
    add tracker update
    
    Signed-off-by: kovacs <mritd@linux.com>

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+TRACKER_URL="https://cdn.staticaly.com/gh/XIU2/TrackersListCollection/master/best_aria2.txt"
+
 if [ ! -f /data/conf/aria2.conf ]; then
     mkdir -p /data/conf
-    cp /etc/aria2.conf /data/conf
+    touch /data/conf/aria2.session
+    cp /etc/aria2.conf /data/conf/aria2.conf
+    sed -i "s@bt-tracker=@bt-tracker=$(curl -sSL ${TRACKER_URL})@g" /data/conf/aria2.conf
 fi
 
 exec aria2c --conf-path=/data/conf/aria2.conf
