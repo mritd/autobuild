495dd15afc998bac69136ca2afa9297a3abf3ac9
kovacs <mritd@linux.com>
2023-05-16 10:16:42 +0800

    chore(build.sh): add alpine build check
    add alpine build check
    
    Signed-off-by: kovacs <mritd@linux.com>

--- build.sh
+++ build.sh
@@ -18,11 +18,28 @@ if [ "${TARGET}" == "alpine" ] || [ "${TARGET}" == "all" ] || [ "${TARGET}" == "
         echo "Alpine workflow triggered with no response"
     fi
 
-    # 等待5秒钟，确保alpine action已经完成
-    sleep 5
+    while true; do
+        # 从 GitHub API 获取 action 的状态
+        ALPINE_STATUS=$(curl -s -H "Authorization: token $TOKEN" \
+            "https://api.github.com/repos/$REPO/actions/workflows/alpine.yml/runs" | \
+            jq -r '.workflow_runs[0].status')
+      
+        # 打印日志
+        echo "Status of autobuild/alpine action: $ALPINE_STATUS"
+      
+        # 如果状态为 "completed"，退出循环
+        if [ "$ALPINE_STATUS" == "completed" ]; then
+            break
+        fi
+      
+        # 等待 3 秒后再次检测
+        sleep 3
+    done
+
 fi
 
 if [ "${TARGET}" == "all" ] || [ "${TARGET}" == "" ]; then
+
     # 获取除alpine action和.earthly以外的所有workflow的名称和ID
     WORKFLOWS=$(curl -s -X GET -H "Authorization: token $TOKEN" \
         "https://api.github.com/repos/$REPO/actions/workflows" | \
